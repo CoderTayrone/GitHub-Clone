b1028d91b43bdc0f4830d0a3fbd99234801bb6cf
CoderTayrone <natsugamer21@gmail.com>
2021-09-05 18:23:35 -0300

    Auth and Search

---
+++ src/Contexts/SearchContext.js
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from 'react'
+
+// Criar Variabel Global
+
+const SearchContext = createContext({});
+
+export const SelectSearch = ({children}) => {
+
+  const [search, setSearch] = useState();
+
+  console.log(search)
+
+  return (
+    <SearchContext.Provider value={{setSearch, search}}>
+      {children}
+    </SearchContext.Provider>
+  )
+}
+
+export function useGetSearch(){
+  const context = useContext(SearchContext);
+
+  if(!context) {
+    console.log('Usando ');
+  }
+
+  return context
+}
